cdd896433a831a4f34b9bdae72729a3d379b03c3
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-08 20:57:27 +0100

    feat(lab-1c): Add basic routes to server

--- 01_node/playtime/src/server.js
+++ 01_node/playtime/src/server.js
@@ -9,8 +9,25 @@ const __dirname = path.dirname(__filename);
 async function init() {
      const server = Hapi.server({
           port: 3000,
-          host: "loc∏alhost",
+          host: "localhost",
      });
+
+     server.route({
+        method: "GET",
+        path: "/",
+        handler: function (request, h) {
+            return "Hello World!";
+        }
+     })
+
+     server.route({
+        method: "GET",
+        path: "/test",
+        handler: function (request, h) {
+            return "Testing a different route...";
+        }
+     });
+
      await server.start();
      console.log("Server running on %s", server.info.uri);
 }
