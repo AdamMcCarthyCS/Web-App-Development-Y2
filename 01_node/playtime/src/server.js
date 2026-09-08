c246219f2c2e93a9e1fa3ca6460a25b447a61397
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-08 20:47:17 +0100

    style(lab-1b): Add linting and code formating to project

--- 01_node/playtime/src/server.js
+++ 01_node/playtime/src/server.js
@@ -1,23 +1,23 @@
-import Hapi from "@hapi/hapi"
-import path from "path"
+import Hapi from "@hapi/hapi";
+import path from "path";
 
-import { fileURLToPath } from "url"
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
 
 async function init() {
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
-    })
-    await server.start()
-    console.log("Server running on %s", server.info.uri)
+     const server = Hapi.server({
+          port: 3000,
+          host: "loc∏alhost",
+     });
+     await server.start();
+     console.log("Server running on %s", server.info.uri);
 }
 
 process.on("unhandledRejection", (err) => {
-    console.log(err);
-    process.exit(1)
-})
+     console.log(err);
+     process.exit(1);
+});
 
-init()
+init();
