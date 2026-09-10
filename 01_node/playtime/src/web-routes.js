b8d0ede63f5dfb9185e08422950409f6d6448dbc
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-10 18:35:25 +0100

    feat(lab-1e): Add main controller and update routes

--- 01_node/playtime/src/web-routes.js
+++ 01_node/playtime/src/web-routes.js
@@ -1,7 +1,9 @@
 import { dashboardController } from "./controllers/dashboard-controller.js";
 import { aboutController } from "./controllers/about-controller.js"
+import { mainController } from "./controllers/main-controller.js"
 
 export const webRoutes = [
-     { method: "GET", path: "/", config: dashboardController.index },
+     { method: "GET", path: "/dashboard", config: dashboardController.index },
      { method: "GET", path: "/about", config: aboutController.index },
+     { method: "GET", path: "/", config: mainController.index}
 ];
