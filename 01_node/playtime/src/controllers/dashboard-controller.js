77a56c16b5be4083a59a0ad1d3f4241962bfa262
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-10 19:19:58 +0100

    feat(lab-1f, exercise-2): Add about page with active highlight

--- 01_node/playtime/src/controllers/dashboard-controller.js
+++ 01_node/playtime/src/controllers/dashboard-controller.js
@@ -1,7 +1,7 @@
 export const dashboardController = {
      index: {
           handler: async function (request, h) {
-               return h.view("dashboard")
+               return h.view("dashboard");
           },
      },
 };
--- 01_node/playtime/src/controllers/main-controller.js
+++ 01_node/playtime/src/controllers/main-controller.js
@@ -1,7 +1,7 @@
 export const mainController = {
     index: {
         handler: async function (request, h) {
-            return h.view("main")
+            return h.view("main");
         }
     }
 }
