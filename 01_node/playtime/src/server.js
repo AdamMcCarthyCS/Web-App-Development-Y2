7352890f26e3c8da6374597c1324205dd39323e7
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-11 19:30:23 +0100

    feat(lab-1g): Import and initialise data store in server

--- 01_node/playtime/src/server.js
+++ 01_node/playtime/src/server.js
@@ -4,6 +4,7 @@ import Handlebars from "handlebars";
 import path from "path";
 import { fileURLToPath } from "url";
 import { webRoutes } from "./web-routes.js";
+import { db } from "./models/db.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -25,8 +26,8 @@ async function init() {
           layout: true,
           isCached: false,
      });
+     db.init()
      server.route(webRoutes);
-
      await server.start();
      console.log("Server running on %s", server.info.uri);
 }
