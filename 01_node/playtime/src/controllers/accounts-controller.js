2099a8f98f581e00c5d8c5a27d16854c3b9ffe32
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-13 17:28:09 +0100

    feat(lab-2f): Add validation method and login cookie handling

--- 01_node/playtime/src/controllers/accounts-controller.js
+++ 01_node/playtime/src/controllers/accounts-controller.js
@@ -34,6 +34,7 @@ export const accountsController = {
             if (!user || user.password !== password) {
                 return h.redirect("/");
             }
+            request.cookieAuth.set({ id: user._id })
             return h.redirect("/dashboard")
         },
     },
@@ -42,5 +43,13 @@ export const accountsController = {
         handler: function (request, h) {
             return h.redirect("/")
         }
+    },
+
+    async validate (request, session) {
+        const user = await db.userStore.getUserById(session.id);
+        if (!user) {
+            return { isValid: false};
+        }
+        return { isValid: true, credentials: user}
     }
 }
