ca78ced6ce7d2be506583c1d26e14c579feb5cb7
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-14 11:17:46 +0100

    feat(lab2-exercise4): Clear cookie on logout

--- 01_node/playtime/src/controllers/accounts-controller.js
+++ 01_node/playtime/src/controllers/accounts-controller.js
@@ -47,6 +47,7 @@ export const accountsController = {
     logout: {
         auth: false,
         handler: function (request, h) {
+            request.cookieAuth.clear()
             return h.redirect("/")
         }
     },
