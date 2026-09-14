1c29d0d2eb9967a97ed4b96a8c794ee49693bfab
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-14 11:48:13 +0100

    refactor(lab2-exercise3): Change how dotenv is imported and config

--- 01_node/playtime/src/server.js
+++ 01_node/playtime/src/server.js
@@ -6,11 +6,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import Cookie from "@hapi/cookie"
+import dotenv from "dotenv"
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js"
 import { accountsController } from "./controllers/accounts-controller.js"
 
-import "dotenv/config"
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
