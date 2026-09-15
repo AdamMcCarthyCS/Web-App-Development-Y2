f4ad19550e0b667cf39863838f8fc10501b3473f
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-15 21:16:03 +0100

    feat(lab-3c): Add validation and schema to signup

--- 01_node/playtime/src/server.js
+++ 01_node/playtime/src/server.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 
 import Cookie from "@hapi/cookie"
 import dotenv from "dotenv"
+import Joi from "joi"
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js"
 import { accountsController } from "./controllers/accounts-controller.js"
@@ -27,6 +28,7 @@ async function init() {
      });
      await server.register(Vision);
      await server.register(Cookie);
+     server.validator(Joi)
 
      server.auth.strategy("session", "cookie", {
      cookie: {
