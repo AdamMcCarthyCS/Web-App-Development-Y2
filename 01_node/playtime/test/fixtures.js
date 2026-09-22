c0b844bfe6e427c770e3dce1ce476a825646b0db
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-22 15:48:36 +0100

    test(lab-5d): Delete all users

--- 01_node/playtime/test/fixtures.js
+++ 01_node/playtime/test/fixtures.js
@@ -4,3 +4,26 @@
     email: "maggie@simpson.com",
     password: "secret",
  }
+
+ export const testUsers = [
+   {
+      firstName: "Homer",
+      lastName: "Simpson",
+      email: "homer@simpson.com",
+      password: "secret",
+   },
+
+   {
+      firstName: "Marge",
+      lastName: "Simpson",
+      email: "marge@simpson.com",
+      password: "secret",
+   },
+   
+   {
+      firstName: "Bart",
+      lastName: "Simpson",
+      email: "bart@simpson.com",
+      password: "secret",
+   },
+ ];
