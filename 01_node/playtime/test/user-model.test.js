81a4a2ee1e2d0b756849610aa2169396c4dbedda
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-23 11:59:54 +0100

    test(lab-5e): Delete a single user

--- 01_node/playtime/test/user-model.test.js
+++ 01_node/playtime/test/user-model.test.js
@@ -39,15 +39,37 @@ suite("User API tests", () => {
             // eslint-disable-next-line no-await-in-loop
             testUsers[i] = await db.userStore.addUser(testUsers[i]);
         }
-        await db.userStore.deleteUserById(testUsers[0]._id)
+        await db.userStore.deleteUserById(testUsers[0]._id);
         const returnedUsers = await db.userStore.getAllUsers();
-        assert.equal(returnedUsers.length, testUsers.length - 1)
+        assert.equal(returnedUsers.length, testUsers.length - 1);
 
         const deletedUser = await db.userStore.getUserById(testUsers[0]._id);
         assert.isNull(deletedUser);
     })
-    
-    
 
-    
+    test("get a user - failures", async () => {
+      const noUserWithId = await db.userStore.getUserById("123");
+      assert.isNull(noUserWithId);
+      const noUserWithEmail = await db.userStore.getUserByEmail("no@one.com");
+      assert.isNull(noUserWithEmail); 
+    })
+
+    test("get a user - bad params", async () => {
+      let nullUser = await db.userStore.getUserByEmail("");
+      assert.isNull(nullUser);
+      nullUser = await db.userStore.getUserById("");
+      assert.isNull(nullUser);
+      nullUser = await db.userStore.getUserById();
+      assert.isNull(nullUser);
+    })
+
+    test("delete one user - fail", async () => {
+      for (let i = 0; i < testUsers.length; i += 1) {
+        // eslint-disable-next-line no-await-in-loop
+        testUsers[i] = await db.userStore.addUser(testUsers[i]);
+      }
+      await db.userStore.deleteUserById("bad-id");
+      const allUsers = await db.userStore.getAllUsers();
+      assert.equal(testUsers.length, allUsers.length);
+    })
 });
