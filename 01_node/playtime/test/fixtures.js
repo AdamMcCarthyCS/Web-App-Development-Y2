9392607031fe97ad7a98d15a25f768fe0c6e570f
AdamMcCarthyCS <adammccarthycs@gmail.com>
2026-09-24 21:55:15 +0100

    feat(lab5-exercise2): Add setup, test retrieving and deleteing playlists

--- 01_node/playtime/test/fixtures.js
+++ 01_node/playtime/test/fixtures.js
@@ -31,23 +31,22 @@
  export const playlist = {
   userid: "61b2a825-8422-4bf7-bcdb-9423a9090852",
   title: "Late Night Drive",
-  _id: "c47e9a21-6b35-4f08-a2d9-73e51bc8640f",
+  tracks: []
 }
 
  export const testPlaylists = [
   {
     userid: "61b2a825-8422-4bf7-bcdb-9423a9090852",
     title: "Test Playlist",
-    _id: "6685c4b3-7f6f-4831-b5d2-0d937aecedee",
+    tracks: []
   },
   {
     userid: "61b2a825-8422-4bf7-bcdb-9423a9090852",
-    title: "Workout Mix",
-    _id: "a3f72c91-5d84-4e16-9b27-6c4f18d0e5a2",
+    title: "Workout Mix"
   },
   {
     userid: "61b2a825-8422-4bf7-bcdb-9423a9090852",
     title: "Chill Evening",
-    _id: "f82b16d4-93a7-4c52-b8e1-25d6a0497c31",
+    tracks: []
   },
 ];
--- 01_node/playtime/test/playlist-model.test.js
+++ 01_node/playtime/test/playlist-model.test.js
@@ -3,9 +3,38 @@ import { db } from "../src/models/db.js"
 import { playlist, testPlaylists } from "./fixtures.js"
 
 suite("Playlist API tests", () => {
+    setup(async () => {
+        db.init();
+        await db.playlistStore.deleteAllPlaylists();
+        for (let i = 0; i < testPlaylists.length; i += 1) {
+            // eslint-disable-next-line no-await-in-loop
+            testPlaylists[i] = await db.playlistStore.addPlaylist(testPlaylists[i])
+        }
+    });
 
     test("create a playlist", async () => {
         const newPlaylist = await db.playlistStore.addPlaylist(playlist);
         assert.equal(playlist, newPlaylist);
     });
+
+    test("get all playlists", async () => {
+        const returnedPlaylists = await db.playlistStore.getAllPlaylists();
+        assert.equal(returnedPlaylists.length, 3);
+    });
+
+    test("delete all playlists", async () => {
+        let returnedPlaylists = await db.playlistStore.getAllPlaylists();
+        assert.equal(returnedPlaylists.length, 3);
+        await db.playlistStore.deleteAllPlaylists();
+        returnedPlaylists = await db.playlistStore.getAllPlaylists();
+        assert.equal(returnedPlaylists.length, 0);
+    });
+
+    test("get a playlist - success", async () => {
+        const addedPlaylist = await db.playlistStore.addPlaylist(playlist);
+        const returnedPlaylist1 = await db.playlistStore.getPlaylistById(addedPlaylist._id);
+        assert.deepEqual(addedPlaylist, returnedPlaylist1);
+    });
+
+
 })
